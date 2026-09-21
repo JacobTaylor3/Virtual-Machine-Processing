4f21ad5a613e843ebb56f018aec3fc00f1fb2e6a
JacobTaylor3 <jacobtaylor048@gmail.com>
2026-09-21 00:14:51 -0400

    gitignore

--- code_writer.py
+++ code_writer.py
@@ -15,8 +15,44 @@ class CodeWriter:
 
         self.asm_out = open(filename, "w")
 
-    def write_arithmetic(self):
+    def write_arithmetic(self):  # called only when the Command instruction is arthmetic
+
         pass
 
-    def write_push_pop(self):
+    def write_push(self):  #
+
+       arg1 = self.parser_in.get_arg1()
+       arg2 = self.parser_in.get_arg2(), # this is an int 
+
+
+        # below is the default push command, we just need to first load the desired value into the D register first, which changes based on what type it is, eg., local,constant,etc
+
+        # @SP
+        # A=M
+        # M=D
+        # @SP
+        # M=M+
+
+        
+
+    def write_pop (self):
         pass
+
+    def run(self):
+
+        while self.parser_in.has_more_commands():
+
+            self.parser_in.advance()
+
+            if self.parser_in.command_type() == "C_POP":
+                self.write_pop()
+
+            elif self.parser_in.command_type() == "C_PUSH":
+                self.write_push()
+
+            elif self.parser_in.command_type() == "C_ARITHMETIC":
+                self.write_arithmetic()
+
+            #will add the function stuff in chapter 8
+
+
